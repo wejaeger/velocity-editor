9c7042c1271ae682d6524c78929825f44f230a19
werner <werner@3e7c82c7-2f88-4090-b006-87e094a7580b>
2011-04-26 00:53:13 +0000

    Released version 2.2.3

---
+++ tags/2.2.3/src/com/tsi/netbeans/modules/languages/velocity/editor/fold/VTLFoldAnalyser.java
@@ -0,0 +1,172 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2009 T-Systems International GmbH.
+ * All rights reserved.
+ * This software is the confidential and proprietary information
+ * of T-Systems International GmbH.
+ *
+ */
+package com.tsi.netbeans.modules.languages.velocity.editor.fold;
+
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.Token;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.VelocityParserConstants;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.ASTElseIfStatement;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.ASTElseStatement;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.ASTForEachStatement;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.ASTIfStatement;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.ASTMacroStatement;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.SimpleNode;
+import com.tsi.netbeans.modules.languages.velocity.jcclexer.node.VelocityAnalyser;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+import org.netbeans.api.editor.fold.Fold;
+import org.netbeans.api.editor.fold.FoldType;
+import org.netbeans.spi.editor.fold.FoldHierarchyTransaction;
+import org.netbeans.spi.editor.fold.FoldOperation;
+import org.openide.text.NbDocument;
+
+/**
+ * Analyzes an abstract syntax tree to find and add code folds to VTL editor.
+ *
+ * @author <a href="mailto:werner.jaeger@t-systems.com">Werner Jaeger</a>
+ */
+class VTLFoldAnalyser extends VelocityAnalyser
+{
+   private final FoldOperation          m_Operation;
+   private final Map<VTLFoldInfo, Fold> m_CurrentFolds;
+   private final StyledDocument         m_Document;
+
+   private FoldHierarchyTransaction     m_Transaction;
+
+   /**
+    * Creates new {@code VTLFoldAnalyser}.
+    *
+    * @param operation a reference to the fold operation this analyzer acts on.
+    *        must not be {@code null}.
+    * @param currentFolds a reference to a map of currently added folds.
+    *        Must not be {@code null}.
+    */
+   VTLFoldAnalyser(final FoldOperation operation, final Map<VTLFoldInfo, Fold> currentFolds)
+   {
+      m_Operation    = operation;
+      m_CurrentFolds = currentFolds;
+      m_Document     = (StyledDocument)m_Operation.getHierarchy().getComponent().getDocument();
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public Object visit(final ASTMacroStatement node, final Object oData)
+   {
+      return(visitImpl(node, oData));
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public Object visit(final ASTForEachStatement node, final Object oData)
+   {
+      return(visitImpl(node, oData));
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public Object visit(final ASTIfStatement node, final Object oData)
+   {
+      return(visitImpl(node, oData));
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public Object visit(final ASTElseIfStatement node, final Object oData)
+   {
+      return(visitImpl(node, oData));
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public Object visit(final ASTElseStatement node, final Object oData)
+   {
+      return(visitImpl(node, oData));
+   }
+
+   private Object visitImpl(final SimpleNode node, final Object oData)
+   {
+      final Token  firstToken = node.getFirstToken();
+      final Token  lastToken  = node.getLastToken();
+
+      if (lastToken.kind == VelocityParserConstants.END)
+      {
+         final int    iStart  = NbDocument.findLineOffset(m_Document, firstToken.beginLine - 1) + firstToken.beginColumn - 1;
+         final int    iEnd    = NbDocument.findLineOffset(m_Document, lastToken.endLine - 1) + lastToken.endColumn - (lastToken.image.length() - lastToken.image.trim().length());
+         final String strDesc = firstToken.image + firstToken.next.image + firstToken.next.next.image + " ...";
+
+         try
+         {
+            final VTLFoldInfo info = new VTLFoldInfo(m_Document, iStart, iEnd, false);
+            final Fold oldFold     = m_CurrentFolds.get(info);
+
+            if (oldFold == null)
+            {
+               final Fold fold = m_Operation.addToHierarchy(new FoldType(firstToken.image), strDesc, false, iStart, iEnd, 0, 0, info, m_Transaction);
+               m_CurrentFolds.put(info, fold);
+            }
+            else
+               ((VTLFoldInfo)m_Operation.getExtraInfo(oldFold)).setState(VTLFoldInfo.State.OLD);
+         }
+         catch (BadLocationException ble)
+         {
+            Logger.getLogger(VTLFoldAnalyser.class.getName()).log(Level.WARNING, null, ble);
+         }
+      }
+
+      return(node.childrenAccept(this, oData));
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public void openTransaction()
+   {
+      if (m_Transaction == null)
+         m_Transaction = m_Operation.openTransaction();
+
+      for (final Fold fold : m_CurrentFolds.values())
+         ((VTLFoldInfo)m_Operation.getExtraInfo(fold)).setState(VTLFoldInfo.State.UNTOUCHED);
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override public void commitTransaction()
+   {
+      final Set<VTLFoldInfo> untouched = new HashSet<VTLFoldInfo>();
+
+      for (final Fold fold : m_CurrentFolds.values())
+      {
+         final VTLFoldInfo info = ((VTLFoldInfo)m_Operation.getExtraInfo(fold));
+         if (info.getState() == VTLFoldInfo.State.UNTOUCHED)
+         {
+            m_Operation.removeFromHierarchy(fold, m_Transaction);
+            untouched.add(info);
+         }
+      }
+
+      for (final VTLFoldInfo info : untouched)
+         m_CurrentFolds.remove(info);
+
+      if (m_Transaction != null)
+         m_Transaction.commit();
+
+      m_Transaction = null;
+   }
+}
---
+++ tags/2.2.3/src/com/tsi/netbeans/modules/languages/velocity/jcclexer/node/VelocityAnalyser.java
@@ -0,0 +1,259 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2009 T-Systems International GmbH.
+ * All rights reserved.
+ * This software is the confidential and proprietary information
+ * of T-Systems International GmbH.
+ *
+ */
+package com.tsi.netbeans.modules.languages.velocity.jcclexer.node;
+
+/**
+ * Visitor base Implementation that servers as base class for concrete
+ * analyzers.
+ *
+ * <p>
+ *    Each {@code visit} method implementation takes no actions beside
+ *    traversing the child nodes.
+ * </p>
+ * @author <a href="mailto:werner.jaeger@t-systems.com">Werner Jaeger</a>
+ */
+abstract public class VelocityAnalyser implements VelocityParserVisitor
+{
+   /**
+    * Creates new {@code VelocityAnalyser}.
+    */
+   public VelocityAnalyser()
+   {
+   }
+
+   @Override public Object visit(final SimpleNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTprocess node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTEscapedDirective node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTEscape node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTComment node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTUnparsedContent node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTFloatingPointLiteral node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTIntegerLiteral node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTStringLiteral node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTIdentifier node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTWord node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTDirective node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTBlock node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTMap node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTObjectArray node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTIntegerRange node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTMethod node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTReference node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTTrue node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTFalse node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTText node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTForEachStatement node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTMacroStatement node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTIncludeStatement node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTIfStatement node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTElseStatement node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTElseIfStatement node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTSetDirective node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTStop node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTExpression node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTAssignment node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTOrNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTAndNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTEQNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTNENode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTLTNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTGTNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTLENode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTGENode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTAddNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTSubtractNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTMulNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTDivNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTModNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   @Override public Object visit(final ASTNotNode node, final Object oData)
+   {
+      return(node.childrenAccept(this, oData));
+   }
+
+   abstract public void openTransaction();
+
+   abstract public void commitTransaction();
+}
